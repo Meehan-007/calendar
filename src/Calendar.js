3affa515d5078b66db51bb21a3169eb2dc088036
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2024-02-09 13:59:57 -0500

    inital commit

---
+++ src/Calendar.js
@@ -0,0 +1,17 @@
+// need bootstrap link 
+
+
+
+// need functions for local storage 
+
+
+// need use effects to automaticaly save it
+// color code , heavy grey for past 
+// white for present 
+// light grey for future  
+// local storage to hold the info in an object 
+// 
+
+
+// for loop to auto generate the fields?
+
