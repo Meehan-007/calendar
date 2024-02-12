524b853092900d2909857d2596dab74d2996d5e2
Meehan-007 <93944358+Meehan-007@users.noreply.github.com>
2024-02-12 14:57:38 -0500

    set up the structure for pages and componets

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,17 +1,45 @@
-// need bootstrap link 
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TimeRow from './Componets/Time';
+
+const Calendar = () => {
+
+    const [hours, setHours] = useState([
+        { hour: '9:00 AM', text: ''}, 
+        { hour: '10:00 AM', text: ''},  
+        { hour: '11:00 AM', text: ''},  
+        { hour: '12:00 PM', text: ''},  
+        { hour: '1:00 PM', text: ''}, 
+        { hour: '2:00 PM', text: ''},  
+        { hour: '3:00 PM', text: ''},  
+        { hour: '4:00 PM', text: ''}, 
+        { hour: '5:00 PM', text: ''} 
+     ]);
+
+    // need functions for local storage 
+
+    // need use effects to automaticaly save it
+    // color code , heavy grey for past 
+    // white for present 
+    // light grey for future  
+    // local storage to hold the info in an object 
+
+
+    // for loop to auto generate the fields?
+    return(
+        <section className='container'> 
+          
+          <TimeRow hours={hours} />
+
+        </section>
+    )
 
 
 
-// need functions for local storage 
 
 
-// need use effects to automaticaly save it
-// color code , heavy grey for past 
-// white for present 
-// light grey for future  
-// local storage to hold the info in an object 
-// 
 
 
-// for loop to auto generate the fields?
+}
 
+export default Calendar; 
--- src/componets/time.js
+++ src/componets/time.js
@@ -1,4 +1,31 @@
-// need boostrap 
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const TimeRow = ({ hours }) => {
+
+
+
+
+    return (
+        <section>
+            {hours.map(hour => ( 
+            <div>  
+           <div key={hour.id}> 
+            {hour.hour}
+           </div>  
+
+           <div key={hour.id + '_textarea'}> 
+             <textarea>  {hour.text} </textarea>
+           </div> 
+           </div>
+
+            ))}
+        </section>
+    );
+}
+
+export default TimeRow;
 
 
 
